6fa63941d2c2bfde64bd69114a05a2f1d91af1ee
Hammad Ghazi <hamamdghazi007@gmail.com>
2023-10-09 22:43:45 +0500

    import OrderEngine lib and attach it with Order struct

--- src/AdvancedOrderEngine.sol
+++ src/AdvancedOrderEngine.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+import {OrderEngine} from "./libraries/OrderEngine.sol";
+
 contract AdvancedOrderEngine {
+    using OrderEngine for OrderEngine.Order;
+
     function fillOrders() external {}
 }
