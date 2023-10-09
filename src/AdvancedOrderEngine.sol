b85a44b5c9d94bc54f5be7b029fe21d7acfcca08
Hammad Ghazi <hamamdghazi007@gmail.com>
2023-10-09 22:54:20 +0500

    create fn stubs as comments

--- src/AdvancedOrderEngine.sol
+++ src/AdvancedOrderEngine.sol
@@ -11,5 +11,20 @@ contract AdvancedOrderEngine {
         uint256[] calldata clearingPrices,
         bytes calldata facilitatorInteractionCalldata,
         address facilitatorInteractionTargetContract
-    ) external {}
+    ) external {
+        // Perform sanity checks (for example, orders and clearing prices array must have same length, etc)
+        // Loop start
+        // Perform order specific sanity checks
+        // Verify signatjures
+        // Verify predicates
+        // Call pre-interaction hook
+        // Transfer funds from maker to vault
+        // Loop end
+        // Call facilitator interaction
+        // Loop start
+        // Ensure facilitator is respecting maker price
+        // Transfer funds from vault to maker
+        // Call post-interaction hook
+        // Emit event (decide where to emit event, as its considered as an effect so maybe do it somewhere in the start)
+    }
 }
