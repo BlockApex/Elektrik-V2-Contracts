301081699323dec10996c57af5b38994e0a51108
Hammad Ghazi <hamamdghazi007@gmail.com>
2023-10-16 20:53:48 +0500

    refactor: modified facilitator interaction hook interface

--- src/interfaces/IInteractionNotificationReceiver.sol
+++ src/interfaces/IInteractionNotificationReceiver.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.21;
 
+import {OrderEngine} from "../libraries/OrderEngine.sol";
+
 /**
  * @notice Interface for facilitator interaction hook, it is invoked after funds are transferred from the 'maker' to the 'vault'.
  */
@@ -9,16 +11,14 @@ interface IInteractionNotificationReceiver {
     /**
      * @notice Callback method that gets called after funds transfer from the 'maker' to the 'vault
      * @param operator Address of the caller who executed orders on behalf of the facilitator.
-     * @param maker Address of the oorder maker
-     * @param orderSellAmount Amount of the asset the maker is willing to sell.
-     * @param amountOffered Amount of the asset the facilitator is willing to pay in exchange for the sell amount.
+     * @param orders Address of the oorder maker
+     * @param offeredAmounts Amounts of the asset the facilitator is offering to the makers.
      * @param interactionData Interaction calldata
      */
     function fillOrderInteraction(
         address operator,
-        address maker,
-        uint256 orderSellAmount,
-        uint256 amountOffered,
+        OrderEngine.Order[] calldata orders,
+        uint256[] calldata offeredAmounts,
         bytes memory interactionData
     ) external;
 }
